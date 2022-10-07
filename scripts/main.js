1a84deccae6648c62b14de2b97fa08a01d6a6549
Daniel Ohrin <xqiam@outlook.com>
2022-10-07 10:23:54 -0500

    Initial

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+class chassis {
+    constructor() { // Has to be called constructor
+        this.metal = "Steel"
+        this.type = "Racecar"
+        this.model = "Nissan"
+    }
+}
+
+console.log(new chassis()) // Parenthesis not "required", but good practice
